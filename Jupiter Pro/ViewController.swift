c3422e2b765658e68186dca83988e909121f2909
Hosea <desktop@Hoseas-Mac-mini.attlocal.net>
2015-01-28 22:16:23 -0500

    Initial Commit

---
+++ Jupiter Pro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jupiter Pro
+//
+//  Created by Hosea on 1/28/15.
+//  Copyright (c) 2015 Glass Sources. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
